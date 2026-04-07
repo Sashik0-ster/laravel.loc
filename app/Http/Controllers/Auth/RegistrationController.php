74125b24cc7a45484c5df92888c82fd76bfbdf1e
Sashik0-ster <124570370+Sashik0-ster@users.noreply.github.com>
2026-04-07 08:08:37 +0200

    Update RegistrationController.php

--- app/Http/Controllers/Auth/RegistrationController.php
+++ app/Http/Controllers/Auth/RegistrationController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\Auth\RegistrationRequest;
+use App\Http\Requests\Auth\RegistrationRequest;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
