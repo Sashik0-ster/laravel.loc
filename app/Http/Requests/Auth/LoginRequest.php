fefb28640cddca1cdd8366b52d9d0daf59f3b97a
Sashik0ster <Sashik0mmm@gmail.com>
2026-04-07 07:22:33 +0200

    Додав фунцію додавання запису доходу до бази данних

---
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'string', 'email'],
+            'password' => ['required', 'string'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'email.required' => 'Поле  обов’язкове для заповнення.',
+            'email.email' => 'Введіть коректну адресу електронної пошти.',
+            'password.required' => 'Поле обов’язкове для заповнення.',
+        ];
+    }
+
+}
+
