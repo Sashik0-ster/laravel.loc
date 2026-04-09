8ea2976cf226797299ed87352dc205932668a45e
Sashik0ster <Sashik0mmm@gmail.com>
2026-04-09 19:26:06 +0200

    Add bootstrap style

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -5,12 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link
         href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700;800&family=Inter:wght@400;500;600&display=swap"
         rel="stylesheet">
-    <link rel="stylesheet" href="@vite(['resources/css/main.css'])">
+    @vite(['resources/css/main.css','resources/css/app.css', 'resources/js/app.js'])
 
     <title>{{'Мій Додаток' }}</title>
 
@@ -19,7 +17,7 @@
 <body>
 
 <div>
-    <x-sidebar :items="$sidebarItems" />
+    <x-sidebar :items="$sidebarItems"/>
 </div>
 
 <main class="main">
@@ -36,8 +34,6 @@
 
 </main>
 
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
 
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,21 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/main.css'],
+            input: [
+                'resources/js/app.js',
+                'resources/css/main.css',
+                'resources/css/app.css',
+            ],
             refresh: true,
         }),
-        tailwindcss(),
     ],
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
         },
     },
 });
